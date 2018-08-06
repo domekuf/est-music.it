305077a168524e424e968ccc57d930c7ef2e061c
Domenico Di Iorio <dome.diiorio@gmail.com>
2018-08-06 23:41:40 +0200

    Updated letters and photos, cleanup some files

--- view/home.php
+++ view/home.php
@@ -1,18 +1,14 @@
-<div class="row h-100 align-items-center text-center" id="home-main">
-    <div class="offset-3 offset-sm-0 offset-md-3">
-    </div>
+<div class="row h-90 align-items-center text-center" id="home-main">
     <?php
     foreach (str_split("est") as $letter) {
     ?>
-    <div class="col-2 col-sm-4 col-md-2">
+    <div class="col-12 col-sm-4">
         <div class="letter">
-            <img src="<?=RT?>/img/est-<?=$letter?>-hover.png" class="hover">
-            <img src="<?=RT?>/img/est-<?=$letter?>.png" class="top">
+            <img src="<?=RT?>/img/est-<?=$letter?>-hover.png?v=<?=md5_file("img/est-$letter-hover.png")?>" class="hover">
+            <img src="<?=RT?>/img/est-<?=$letter?>.png?v=<?=md5_file("img/est-$letter.png")?>" class="top">
         </div>
     </div>
     <?php
     }
     ?>
-    <div class="offset-3 offset-sm-0 offset-md-3">
-    </div>
 </div>
