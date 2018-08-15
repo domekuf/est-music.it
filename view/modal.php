04f69b1a43ecf0bd5edcf53e422f468bbd381b13
Domenico Di Iorio <dome.diiorio@gmail.com>
2018-08-15 22:32:24 +0200

    Added modal

---
+++ view/modal.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="<?=$id?>" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <h5><?=$title?></h5>
+                  <a role="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <i class="fa fa-times"></i>
+                  </a>
+              </div>
+              <div class="modal-body text-justify">
+              <?=$content?>
+              </div>
+              <?php if ($footer_visible) { ?>
+              <div class="modal-footer">
+              </div>
+              <?php } ?>
+          </div>
+    </div>
+</div>
